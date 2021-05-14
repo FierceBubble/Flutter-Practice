8db760d9b48929344a1456a30d54fcfe538cce9e
FierceBubble <83863427+FierceBubble@users.noreply.github.com>
2021-05-14 15:42:02 +0800

    Added button and button action

--- lib/main.dart
+++ lib/main.dart
@@ -19,24 +19,42 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String text = "";
+
+  void changeText(String text) {
+    this.setState(() {
+      this.text = text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Hello World!')),
-      body: InputWidget(),
+      body: Column(
+        children: <Widget>[InputWidget(this.changeText), Text(this.text)],
+      ),
     );
   }
 }
 
 class InputWidget extends StatefulWidget {
+  final Function(String) callback;
+
+  InputWidget(this.callback);
+
   @override
   _InputWidgetState createState() => _InputWidgetState();
 }
 
 class _InputWidgetState extends State<InputWidget> {
   final controller = TextEditingController();
-  String text = "";
 
   @override
   void dispose() {
@@ -44,26 +62,27 @@ class _InputWidgetState extends State<InputWidget> {
     controller.dispose();
   }
 
-  void changeText(text) {
-    if (text == "Hello World!") {
-      controller.clear();
-      text = "";
-    }
-    setState(() {
-      this.text = text;
-    });
+  void click() {
+    widget.callback(controller.text);
+    controller.clear();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      TextField(
+    return TextField(
         controller: this.controller,
         decoration: InputDecoration(
-            prefixIcon: Icon(Icons.message), labelText: "Type a message:"),
-        onChanged: (text) => changeText(text),
-      ),
-      Text(this.text)
-    ]);
+            prefixIcon: Icon(Icons.message),
+            labelText: "Type a message:",
+            suffixIcon: IconButton(
+              icon: Icon(Icons.send),
+              //button icon
+              splashColor: Colors.red,
+              //shows indication color when user press the button
+              tooltip: "Post message",
+              //shows message when user long press the button
+              onPressed: this.click,
+              //action when button is pressed
+            )));
   }
 }
